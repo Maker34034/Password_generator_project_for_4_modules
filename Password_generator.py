506f37bca9dd70307b38f71a80e3c42a76046bc5
Maker34034 <mihailscorpion339@gmail.com>
2024-08-29 11:43:00 +0300

    2 ver

--- Password_generator.py
+++ Password_generator.py
@@ -10,7 +10,7 @@ def description():
     print("- буквы, цифры и спецсимволы.")
     print("3. Программа умеет создавать пять типов паролей по условиям.")
     print('Чтобы выйти из программы, нужно в строке "Введите номер типа пароля:" написать "stop"')
-    
+
 def Types_of_password(): 
     print()
     print('1 - Буквы разного регистра и цифры') 
@@ -19,75 +19,50 @@ def Types_of_password():
     print('4 - Только буквы разного регистра')
     print('5 - Буквы, цифры и спецсимволы') 
 
-def generate_random_password(length):
-    chars = '+-/*!&$#?=@<>[]{}''"":;`~^()abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' 
-    password =''                                
-    for i in range(int(length)):                 
+def generate_password(length, chars):
+    password =''                                 
+    for i in range(int(length)):                   
         password += random.choice(chars)         
-    print('Сгенерированный пароль:', password)  
-
-def generate_password_only_number(length):
-    chars = '1234567890'
-    password =''    
-    for i in range(int(length)):
-        password += random.choice(chars)
-    print('Сгенерированный пароль:', password)
-
-def generate_password_only_specal_simvols(length):
-    chars = '+-/*!&$#?=@<>[]{}''"":;`~^()'
-    password =''    
-    for i in range(int(length)):
-        password += random.choice(chars)
-    print('Сгенерированный пароль:', password)
-
-def generate_password_with_letters_only(length):
-    chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    password =''    
-    for i in range(int(length)):
-        password += random.choice(chars)
-    print('Сгенерированный пароль:', password)
-
-def generate_password_with_letters_and_numbers(length):
-    chars = '1234567890abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    password =''    
-    for i in range(int(length)):
-        password += random.choice(chars)
-    print('Сгенерированный пароль:', password)    
+    print('Сгенерированный пароль:', password)   
 
 def main():  
-    print()        
+    print()           
     description() 
+
     while True: 
         print()
         length = input('Введите длину пароля: ') 
+
         if int(length) <= 0: 
             print('Длина пароля должна быть положительным числом. Попробуйте снова.')
             continue
-        
-        
+
         Types_of_password() 
-        
         print()
-        password_type = input("Введите номер типа пароля: ")
+        password_type = input("Введите номер типа пароля: ") 
         if password_type == 'stop': 
             break        
-        
-        if password_type == '1':
-            password = generate_password_with_letters_and_numbers(length)
-        
+
+        if password_type == '1': 
+            chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' 
+            generate_password(length, chars)
+
         elif password_type == '2':
-            password = generate_password_only_number(length)
-        
+            chars = '1234567890' 
+            generate_password(length, chars)
+
         elif password_type == '3':
-            password = generate_password_only_specal_simvols(length)
-        
+            chars = '+-/*!&$#?=@<>[]{}''"":;`~^()' 
+            generate_password(length, chars)
+
         elif password_type == '4':
-            password = generate_password_with_letters_only(length)
-        
+            chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' 
+            generate_password(length, chars)
+
         elif password_type == '5':
-            password = generate_random_password(length)
-        
+            chars = '+-/*!&$#?=@<>[]{}''"":;`~^()abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' 
+            generate_password(length, chars)
+
         else: 
             print("Неверный выбор. Попробуйте снова.") 
-
 main() 
